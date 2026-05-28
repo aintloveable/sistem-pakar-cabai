c857092e55e80a29ea40c8178d430f146dfb4c34
aint.loveable <seconddamsel@gmail.com>
2026-05-28 12:46:57 +0700

    hps login

--- routes/web.php
+++ routes/web.php
@@ -10,45 +10,29 @@ Route::get('/', function () {
     return redirect('/dashboard');
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::resource('/gejala', GejalaController::class);
 
-    Route::resource('/gejala', GejalaController::class);
+Route::resource('/penyakit', PenyakitController::class);
 
-    Route::resource('/penyakit', PenyakitController::class);
+Route::get('/diagnosa', [DiagnosaController::class, 'index']);
 
-    Route::get('/diagnosa', [DiagnosaController::class, 'index']);
+Route::post('/diagnosa', [DiagnosaController::class, 'hasil']);
 
-    Route::post('/diagnosa', [DiagnosaController::class, 'hasil']);
-
-    Route::get('/basis-pengetahuan', function () {
-
+Route::get('/basis-pengetahuan', function () {
     $aturan = \App\Models\Aturan::all();
-
-    return view(
-        'basis-pengetahuan',
-        compact('aturan')
-    );
+    return view('basis-pengetahuan', compact('aturan'));
 });
 
-    Route::get('/riwayat', function () {
-
+Route::get('/riwayat', function () {
     $riwayat = \App\Models\Hasil::latest()->get();
-
     return view('riwayat', compact('riwayat'));
 });
 
 Route::delete('/riwayat/{id}', function ($id) {
-
     \App\Models\Hasil::destroy($id);
-
     return redirect('/riwayat');
-
 });
-
-});
-
-require __DIR__.'/auth.php';
